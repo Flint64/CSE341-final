7f4df680062fc42f646db2df0d9dca7230ccf866
Flint64 <twda96@gmail.com>
2021-03-26 10:52:16 -0600

    removed is alphaNumeric() from ticket post route

--- routes/ticket.js
+++ routes/ticket.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // router.get('/getTickets/:projectId', ticketController.getTickets);
 
 router.post('/postTicket', isAuth, [
-        body('title', 'Title must be at least 5 characters and only text & numbers').isLength({ min: 5 }).isAlphanumeric().trim(),
+        body('title', 'Title must be at least 5 characters').isLength({ min: 5 }).trim(),
         body('description', 'Description must be at least 10 characters').isLength({ min: 10 }).trim(),
         body('projectId', 'Is the project ID correct or missing?').trim(),
         body('userId', 'Is the user ID correct or missing?').trim()
